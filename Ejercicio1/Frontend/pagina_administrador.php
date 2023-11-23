9ba630c0e3ca75c2966805b67cb30bcf8d27e2cb
Michael <149196773+Michael-Castillo@users.noreply.github.com>
2023-11-23 14:46:04 -0500

    Update pagina_administrador.php

--- Ejercicio1/Frontend/pagina_administrador.php
+++ Ejercicio1/Frontend/pagina_administrador.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página del Administrador</title>
-    <link rel="stylesheet" type="text/css" href="../frontend/Estilos/administrador.css">
+    <link rel="stylesheet" type="text/css" href="Estilos/administrador.css">
 </head>
 <body>
     <h1>Bienvenido Administrador</h1>
